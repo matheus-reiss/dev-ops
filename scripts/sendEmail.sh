ab029e5e11583d2442b9cca1a192d8e6733873b2
Matheus Reis <reismatheus514@gmail.com>
2025-09-24 10:57:52 -0300

    fix(scripts): adjust sendEmail.sh to only handle email sending

--- scripts/sendEmail.sh
+++ scripts/sendEmail.sh
@@ -1,4 +1,4 @@
-cd Ci-Cd
-sudo apt-get update
-sudo apt-get install -y mailutils
-echo "Sending e-mail after pipeline completion" | mail -s "Pipeline" reismatheus514@gmail.com
+set -euxo pipefail
+
+echo "Sending e-mail after pipeline completion" \
+  | mail -s "Pipeline" reismatheus514@gmail.com
